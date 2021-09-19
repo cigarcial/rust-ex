2ccb133b9d7bf2c59402e19ab041e6ec11836ce3
Ciro Garcia <cigarcial@unal.edu.co>
2021-09-18 19:46:57 -0500

    Gitignore corrected Gitignore corrected
    A
    A

---
+++ cchain/target/debug/deps/cchain-cc2f4b9129e3e247.d
@@ -0,0 +1,5 @@
+/home/tsukiko/Documentos/rust-ex/cchain/target/debug/deps/cchain-cc2f4b9129e3e247.rmeta: src/main.rs
+
+/home/tsukiko/Documentos/rust-ex/cchain/target/debug/deps/cchain-cc2f4b9129e3e247.d: src/main.rs
+
+src/main.rs:
---
+++ cchain/target/debug/deps/typenum-3d914cc6c365b317.d
@@ -0,0 +1,18 @@
+/home/tsukiko/Documentos/rust-ex/cchain/target/debug/deps/typenum-3d914cc6c365b317.rmeta: /home/tsukiko/.cargo/registry/src/github.com-1ecc6299db9ec823/typenum-1.14.0/src/lib.rs /home/tsukiko/.cargo/registry/src/github.com-1ecc6299db9ec823/typenum-1.14.0/src/bit.rs /home/tsukiko/.cargo/registry/src/github.com-1ecc6299db9ec823/typenum-1.14.0/src/int.rs /home/tsukiko/.cargo/registry/src/github.com-1ecc6299db9ec823/typenum-1.14.0/src/marker_traits.rs /home/tsukiko/.cargo/registry/src/github.com-1ecc6299db9ec823/typenum-1.14.0/src/operator_aliases.rs /home/tsukiko/.cargo/registry/src/github.com-1ecc6299db9ec823/typenum-1.14.0/src/private.rs /home/tsukiko/.cargo/registry/src/github.com-1ecc6299db9ec823/typenum-1.14.0/src/type_operators.rs /home/tsukiko/.cargo/registry/src/github.com-1ecc6299db9ec823/typenum-1.14.0/src/uint.rs /home/tsukiko/.cargo/registry/src/github.com-1ecc6299db9ec823/typenum-1.14.0/src/array.rs /home/tsukiko/Documentos/rust-ex/cchain/target/debug/build/typenum-653901a14a8a16bb/out/op.rs /home/tsukiko/Documentos/rust-ex/cchain/target/debug/build/typenum-653901a14a8a16bb/out/consts.rs
+
+/home/tsukiko/Documentos/rust-ex/cchain/target/debug/deps/typenum-3d914cc6c365b317.d: /home/tsukiko/.cargo/registry/src/github.com-1ecc6299db9ec823/typenum-1.14.0/src/lib.rs /home/tsukiko/.cargo/registry/src/github.com-1ecc6299db9ec823/typenum-1.14.0/src/bit.rs /home/tsukiko/.cargo/registry/src/github.com-1ecc6299db9ec823/typenum-1.14.0/src/int.rs /home/tsukiko/.cargo/registry/src/github.com-1ecc6299db9ec823/typenum-1.14.0/src/marker_traits.rs /home/tsukiko/.cargo/registry/src/github.com-1ecc6299db9ec823/typenum-1.14.0/src/operator_aliases.rs /home/tsukiko/.cargo/registry/src/github.com-1ecc6299db9ec823/typenum-1.14.0/src/private.rs /home/tsukiko/.cargo/registry/src/github.com-1ecc6299db9ec823/typenum-1.14.0/src/type_operators.rs /home/tsukiko/.cargo/registry/src/github.com-1ecc6299db9ec823/typenum-1.14.0/src/uint.rs /home/tsukiko/.cargo/registry/src/github.com-1ecc6299db9ec823/typenum-1.14.0/src/array.rs /home/tsukiko/Documentos/rust-ex/cchain/target/debug/build/typenum-653901a14a8a16bb/out/op.rs /home/tsukiko/Documentos/rust-ex/cchain/target/debug/build/typenum-653901a14a8a16bb/out/consts.rs
+
+/home/tsukiko/.cargo/registry/src/github.com-1ecc6299db9ec823/typenum-1.14.0/src/lib.rs:
+/home/tsukiko/.cargo/registry/src/github.com-1ecc6299db9ec823/typenum-1.14.0/src/bit.rs:
+/home/tsukiko/.cargo/registry/src/github.com-1ecc6299db9ec823/typenum-1.14.0/src/int.rs:
+/home/tsukiko/.cargo/registry/src/github.com-1ecc6299db9ec823/typenum-1.14.0/src/marker_traits.rs:
+/home/tsukiko/.cargo/registry/src/github.com-1ecc6299db9ec823/typenum-1.14.0/src/operator_aliases.rs:
+/home/tsukiko/.cargo/registry/src/github.com-1ecc6299db9ec823/typenum-1.14.0/src/private.rs:
+/home/tsukiko/.cargo/registry/src/github.com-1ecc6299db9ec823/typenum-1.14.0/src/type_operators.rs:
+/home/tsukiko/.cargo/registry/src/github.com-1ecc6299db9ec823/typenum-1.14.0/src/uint.rs:
+/home/tsukiko/.cargo/registry/src/github.com-1ecc6299db9ec823/typenum-1.14.0/src/array.rs:
+/home/tsukiko/Documentos/rust-ex/cchain/target/debug/build/typenum-653901a14a8a16bb/out/op.rs:
+/home/tsukiko/Documentos/rust-ex/cchain/target/debug/build/typenum-653901a14a8a16bb/out/consts.rs:
+
+# env-dep:TYPENUM_BUILD_CONSTS=/home/tsukiko/Documentos/rust-ex/cchain/target/debug/build/typenum-653901a14a8a16bb/out/consts.rs
+# env-dep:TYPENUM_BUILD_OP=/home/tsukiko/Documentos/rust-ex/cchain/target/debug/build/typenum-653901a14a8a16bb/out/op.rs
